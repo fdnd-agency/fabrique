a7d1bd585545408e28ce68d55d7d5597371a00b0
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-10-07 15:17:29 +0200

    title toegevoegd, aria-label

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,74 +1,78 @@
 <script>
-    import Search from '$lib/Search.svelte';
-    let filterText = '';
+	import Search from '$lib/Search.svelte';
+	let filterText = '';
 </script>
 
-
 <div class="wrap">
-    <div class="search">
-       <input type="text" class="searchTerm" bind:value={filterText} placeholder="Search the collection">
-       <button type="submit" class="searchButton">
-         <Search/>
-      </button>
-    </div>
- </div>
+	<div class="search">
+		<input
+			type="text"
+			class="searchTerm"
+			bind:value={filterText}
+			placeholder="Search the collection"
+		/>
+		<button type="submit" class="searchButton" aria-label="Submit search">
+			<Search />
+		</button>
+	</div>
+</div>
 
 <style>
-* {
-    transition: .2s;
-    font-family: 'DIN Next', sans-serif;
-}
+	* {
+		transition: 0.2s;
+		font-family: 'DIN Next', sans-serif;
+	}
 
-.search {
-  width: 100vw;
-  position: relative;
-  display: flex;
-}
+	.search {
+		width: 100vw;
+		position: relative;
+		display: flex;
+	}
 
-.searchTerm {
-  width: 100%;
-  border: 2px solid black;
-  border-right: none;
-  padding: .5em 0 0 .5em;
-  color: #c1c1c1;
-  font-size: 25px;
-  text-decoration: underline black;
-}
+	.searchTerm {
+		width: 100%;
+		border: 2px solid black;
+		border-right: none;
+		padding: 0.5em 0 0 0.5em;
+		color: #707070;
+		font-size: 25px;
+		text-decoration: underline black;
+	}
 
-.searchTerm:focus{
-  color: black;
-}
+	.searchTerm:focus {
+		color: black;
+	}
 
-.searchButton {
-  width: 4em;
-  height: 5.3em;
-  padding: .5em;
-  border-left: none;
-  background: white;
-  text-align: center;
-  justify-content: center;
-  color: black;
-  cursor: pointer;
-}
+	.searchButton {
+		width: 4em;
+		height: 5.3em;
+		padding: 0.5em;
+		border-left: none;
+		background: white;
+		text-align: center;
+		justify-content: center;
+		color: black;
+		cursor: pointer;
+	}
 
-/*Resize the wrap to see the search bar change!*/
-.wrap{
-  position: absolute;
-  bottom: 0;
-}
+	/*Resize the wrap to see the search bar change!*/
+	.wrap {
+		position: absolute;
+		bottom: 0;
+	}
 
-@media only screen and (min-width: 600px) {
-    .wrap{
-  position: absolute;
-  bottom: 4em;
-  left: 2em;
-  right: 2em;
-}
+	@media only screen and (min-width: 600px) {
+		.wrap {
+			position: absolute;
+			bottom: 4em;
+			left: 2em;
+			right: 2em;
+		}
 
-.search {
-  width: 90vw;
-  position: relative;
-  display: flex;
-}
-}
+		.search {
+			width: 90vw;
+			position: relative;
+			display: flex;
+		}
+	}
 </style>
